d8147307d267a60574b8ebbb28f270305ccc8c97
Avi Mancillas <avmancillas@gmail.com>
2021-07-27 20:26:37 -0500

    Updated code

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           <Route path = "/Resume" component= {Resume}/>
           <Route path = "/ContactMe" component= {ContactMe}/>
         </Switch>
+        
 
       </div>
     </Router>
